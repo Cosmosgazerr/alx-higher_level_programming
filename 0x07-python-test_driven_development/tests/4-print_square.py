f7ec12545a6e61bc8c8484a1be25994b606f4f5c
Cosmosgazerr <saad.elbouhi1@gmail.com>
2024-01-08 17:06:39 -0800

    tests

---
+++ 0x07-python-test_driven_development/tests/4-print_square.py
@@ -0,0 +1,25 @@
+"4-print_square.py" [New File]                                                          0,0-1         AllZZ
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+"4-print_square.py" [New File]                                                          0,0-1         All
+
